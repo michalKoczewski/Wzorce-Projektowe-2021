1424e5b5361c72a54c8b6b965ba3697cac79e9d7
kubasse <kubaposadzy98@gmail.com>
2021-06-07 14:43:09 +0200

    Factory

--- WzorceProjektowe/src/Wzorce/Fabryka.java
+++ WzorceProjektowe/src/Wzorce/Fabryka.java
@@ -9,4 +9,67 @@ package Wzorce;
  *
  * @author kubap
  */
+abstract class Computer {
+    public abstract String getRAM();
+    public abstract String getHDD();
+    public abstract String getCPU();
 
+    @Override
+    public String toString() {
+        return "RAM= "+this.getRAM()+", HDD="+this.getHDD()+", CPU="+this.getCPU();
+    }  
+}
+
+class PC extends Computer {
+    private String ram;
+    private String hdd;
+    private String cpu;
+    
+    public PC(String ram, String hdd, String cpu){
+        this.ram = ram;
+        this.hdd = hdd;
+        this.cpu = cpu;
+    }
+
+    @Override
+    public String getRAM() {
+        return this.ram;
+    }
+
+    @Override
+    public String getHDD() {
+        return this.hdd;
+    }
+
+    @Override
+    public String getCPU() {
+        return this.cpu;
+    }  
+}
+
+class Server extends Computer {
+    private String ram;
+    private String hdd;
+    private String cpu;
+    
+    public Server(String ram, String hdd, String cpu){
+        this.ram = ram;
+        this.hdd = hdd;
+        this.cpu = cpu;
+    }
+
+    @Override
+    public String getRAM() {
+        return this.ram;
+    }
+
+    @Override
+    public String getHDD() {
+        return this.hdd;
+    }
+
+    @Override
+    public String getCPU() {
+        return this.cpu;
+    }  
+}
